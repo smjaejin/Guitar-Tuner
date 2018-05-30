526547af75599b636851babb5974b45f612802fe
David Szilagyi <ddsidavid@gmail.com>
2018-05-30 12:39:25 -0700

    working settings+advanced switch is ready without activity

--- app/src/main/java/com/seanmclane/guitar_tuner/SettingsActivity.java
+++ app/src/main/java/com/seanmclane/guitar_tuner/SettingsActivity.java
@@ -2,21 +2,17 @@ package com.seanmclane.guitar_tuner;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -24,6 +20,7 @@ public class SettingsActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private List<Settings> settings;
     private Switch changeBackground;
+    private Switch tuneActivity;
     private TextView toolTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +38,12 @@ public class SettingsActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         Intent i = getIntent();
         Settings backgroundMode = new Settings(false, "Light Mode/Dark Mode" );
+        Settings tuning = new Settings(false, "Advanced/Standard");
         changeBackground = (Switch) findViewById(R.id.switch1);
+        tuneActivity = (Switch) findViewById(R.id.switch1);
         toolTitle = (TextView) findViewById(R.id.textView2);
         settings.add(backgroundMode);
-
+        settings.add(tuning);
 
 //        changeBackground.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 //            @Override
